afc03f3eb5dac923ac391a4007269315cc826a34
leabiv <leabiv@gmail.com>
2021-05-10 17:38:13 -0500

    Actualizacion en Backend de contacto

--- contacto/forms.py
+++ contacto/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class FormularioContacto(forms.Form):
 
     nombre = forms.CharField(label='Nombre', required=True, max_length=100)
-    email = forms.CharField(label='Nombre', required=True, max_length=100)
+    email = forms.CharField(label='Email', required=True, max_length=100)
     contenido = forms.CharField(label='Contenido', widget=forms.Textarea)
--- projectWeb/settings.py
+++ projectWeb/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Configuracion de Email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 586
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
