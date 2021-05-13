52190ae5dda533f14e32697c5d46211f0654d02d
leabiv <leabiv@gmail.com>
2021-05-12 19:22:25 -0500

    Documento app.yaml y ALLOWED_HOSTS

--- projectWeb/settings.py
+++ projectWeb/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@dvd2c(o@a**(dy(3ol3iwuuva0r6sz_akroz0du96+4ycqm6y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
